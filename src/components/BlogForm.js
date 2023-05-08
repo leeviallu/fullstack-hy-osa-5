656f63ec68ce94c4f1a7176cb7fa8c8e79255321
leeviallu <68004427+leeviallu@users.noreply.github.com>
2023-05-08 20:04:05 +0300

    Eslint korjaukset

--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -1,44 +1,44 @@
 import { useState } from 'react'
 
 const BlogForm = ({ createBlog }) => {
-    const [newBlogTitle, setNewBlogTitle] = useState('')
-    const [newBlogAuthor, setNewBlogAuthor] = useState('')
-    const [newBlogUrl, setNewBlogUrl] = useState('')
+  const [newBlogTitle, setNewBlogTitle] = useState('')
+  const [newBlogAuthor, setNewBlogAuthor] = useState('')
+  const [newBlogUrl, setNewBlogUrl] = useState('')
 
-    const addBlog = (event) => {
-        event.preventDefault()
-        createBlog({
-            title: newBlogTitle,
-            author: newBlogAuthor,
-            url: newBlogUrl,
-        })
-        setNewBlogTitle('')
-        setNewBlogAuthor('')
-        setNewBlogUrl('')
-    }
-    return (
-        <form onSubmit={addBlog}>
+  const addBlog = (event) => {
+    event.preventDefault()
+    createBlog({
+      title: newBlogTitle,
+      author: newBlogAuthor,
+      url: newBlogUrl,
+    })
+    setNewBlogTitle('')
+    setNewBlogAuthor('')
+    setNewBlogUrl('')
+  }
+  return (
+    <form onSubmit={addBlog}>
             title:
-            <input
-                value={newBlogTitle}
-                onChange={(event) => setNewBlogTitle(event.target.value)}
-            />
-            <br />
+      <input
+        value={newBlogTitle}
+        onChange={(event) => setNewBlogTitle(event.target.value)}
+      />
+      <br />
             author:
-            <input
-                value={newBlogAuthor}
-                onChange={(event) => setNewBlogAuthor(event.target.value)}
-            />
-            <br />
+      <input
+        value={newBlogAuthor}
+        onChange={(event) => setNewBlogAuthor(event.target.value)}
+      />
+      <br />
             url:
-            <input
-                value={newBlogUrl}
-                onChange={(event) => setNewBlogUrl(event.target.value)}
-            />
-            <br />
-            <button type="submit">save</button>
-        </form>  
-    )
+      <input
+        value={newBlogUrl}
+        onChange={(event) => setNewBlogUrl(event.target.value)}
+      />
+      <br />
+      <button type="submit">save</button>
+    </form>
+  )
 }
 
 export default BlogForm
--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,14 +1,14 @@
 const Notification = ({ message }) => {
-    if (message === null) {
-      return null
-    }
-  
-    return (
-      <div className="error">
-        {message}
-      </div>
-    )
+  if (message === null) {
+    return null
   }
-  
-  
+
+  return (
+    <div className="error">
+      {message}
+    </div>
+  )
+}
+
+
 export default Notification
